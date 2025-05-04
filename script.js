c278ce25f388350c3ca0002aebbf72d04e7a2c25
Ashfaq Ahmed <70107600+Ash-315@users.noreply.github.com>
2025-05-04 04:09:00 +0400

    Write logic to get human choice for game

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function getHumanChoice() {
+    let choice = prompt("1 - Rock, 2 - Paper, 3 - Scissors; Make a choice: ");
+    choice = parseInt(choice);
+
+    if(choice === 1) {
+        return "rock";
+    }
+    else if(choice === 2) {
+        return "paper";
+    }
+    else if(choice === 3) {
+        return "scissors";
+    }
+}
